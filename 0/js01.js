b0d5a08e2b639a378f8fab17059be00061a92b11
Sameep Kulkarni <kulsameep325@gmail.com>
2020-05-30 20:10:04 +0530

    2nd day work

--- 0/js01.js
+++ 0/js01.js
@@ -1,8 +1,8 @@
 // You may make differnt files for differnt purposes.
 // REPORT ABOUT CHANGES AND FILE IN WORK TO WHATSAPP GROUP TO KEEP EVERYONE UPDATED
-function cancel(id) {
-    var y = document.getElementById(id)
-    y.innerHTML = ''
+//1st js file
+var y = document.getElementById(id)
+y.innerHTML = ''
 }
 
 function next(copy, paste) {
