6dfbf1b975ead6ef86752a7c9d26a44a83f966f3
Sameep Kulkarni <kulsameep325@gmail.com>
2020-05-30 20:07:27 +0530

    Co-authored-by: Prahlad Amudan <prahlad2001a@gmail.com> Co-authored-by: apte01 <chaitanya902@gmail.com> Co-authored-by: kart1802 <karthikswami.1802@gmail.com>

--- 0/js01.js
+++ 0/js01.js
@@ -1,26 +1,12 @@
 // You may make differnt files for differnt purposes.
 // REPORT ABOUT CHANGES AND FILE IN WORK TO WHATSAPP GROUP TO KEEP EVERYONE UPDATED
-
-function addedu() {
-    var x = document.getElementById('edu_node');
-    var y = document.getElementById("edu");
-    y.innerHTML = x.innerHTML;
-}
-
 function cancel(id) {
-    var y = document.getElementById(id);
-    y.innerHTML = "";
+    var y = document.getElementById(id)
+    y.innerHTML = ''
 }
 
-
-function addpro() {
-    var x = document.getElementById('pro_node')
-    var y = document.getElementById("pro");
-    y.innerHTML = x.innerHTML;
-}
-
-function addint() {
-    var x = document.getElementById('int_node');
-    var y = document.getElementById("int");
-    y.innerHTML = x.innerHTML;
+function next(copy, paste) {
+    var x = document.getElementById(copy)
+    var y = document.getElementById(paste)
+    y.innerHTML = x.innerHTML
 }
