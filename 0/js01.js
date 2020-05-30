1bf015a44471183ed586cc935fc52601270bb75d
Chaitanya Apte <chaitanya902@gmail.com>
2020-05-30 10:49:51 +0530

    js changes

--- 0/js01.js
+++ 0/js01.js
@@ -1,2 +1,35 @@
 // You may make differnt files for differnt purposes.
 // REPORT ABOUT CHANGES AND FILE IN WORK TO WHATSAPP GROUP TO KEEP EVERYONE UPDATED
+
+
+
+
+
+function add_edu(){
+        var x=document.getElementById("edu_node");
+
+        var node=x.cloneNode(true);
+        document.getElementById("edu_node").appendChild(node);
+
+}
+
+
+
+
+function add_pro(){
+    var x=document.getElementById("pro_node");
+
+    var node=x.cloneNode(true);
+    x.appendChild(node);
+
+}
+
+
+
+function add_int(){
+    var x=document.getElementById("int_node");
+
+    var node=x.cloneNode(true);
+    x.appendChild(node);
+
+}
