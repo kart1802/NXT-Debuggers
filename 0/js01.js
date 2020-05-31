67e75dddaccc4b4cb04b8a74548085e0ea66f253
Sameep Kulkarni <kulsameep325@gmail.com>
2020-05-31 23:14:04 +0530

    Third day work

--- 0/js01.js
+++ 0/js01.js
@@ -1,12 +1,13 @@
 // You may make differnt files for differnt purposes.
 // REPORT ABOUT CHANGES AND FILE IN WORK TO WHATSAPP GROUP TO KEEP EVERYONE UPDATED
-//1st js file
+// 1st js file
 function cancel(e) {
     e.parentNode.parentNode.removeChild(e.parentNode)
 }
 
-function next(copy, paste) {
-    var x = document.getElementById(copy)
-    var y = document.getElementById(paste)
-    y.innerHTML = x.innerHTML
+function next(copy, name) {
+    var x = document.getElementById(copy).firstElementChild
+    var y = x.cloneNode(true)
+    y.innerHTML += '<button type="button" onclick="cancel(this)" class="cancel btn btn-danger btn-lg text-center">Delete ' + name + '</button><br><br>'
+    document.getElementById(copy).appendChild(y)
 }
