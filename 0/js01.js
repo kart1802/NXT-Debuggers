a3b4724222e936cc094ca420b0ca761146199318
Sameep Kulkarni <kulsameep325@gmail.com>
2020-05-31 18:03:30 +0530

    3rd day morning

--- 0/js01.js
+++ 0/js01.js
@@ -1,7 +1,6 @@
 // You may make differnt files for differnt purposes.
 // REPORT ABOUT CHANGES AND FILE IN WORK TO WHATSAPP GROUP TO KEEP EVERYONE UPDATED
 //1st js file
-
 function cancel(e) {
     e.parentNode.parentNode.removeChild(e.parentNode)
 }
